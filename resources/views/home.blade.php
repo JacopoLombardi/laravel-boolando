77016c6aa5fef19324a16700540927872f5fae62
JacopoLombardi <lombajacopo@gmail.com>
2024-05-09 18:45:16 +0200

    [add] completed Main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
                 <div class="card-inside">
 
                     <div class="upper  relative">
-                        
+
                         {{-- img --}}
                         <div>
                             <img src="/img/{{$product['frontImage']}}" alt="brand">
@@ -25,13 +25,27 @@
                         </div>
 
                         {{-- badges --}}
-                        <div class="sconto absolute">
-                            20%
-                        </div>
 
-                        <div class="sostenibile absolute">
-                            Sostenibile
-                        </div>
+                        {{-- logica per avere l'ultimo elemento dell'array type e poi prendere il 'value' e se non ce lo sconto metto stringa vuota --}}
+                        <?php
+                            $last_array = array_key_last($product['badges']);
+                            $discount = $product['badges'][$last_array]['value'];
+                            if($discount === 'Sostenibilità'){
+                                $discount = '';
+                            }
+                        ?>
+
+                        @if (!($discount === ''))
+                            <div class="sconto absolute">
+                                {{ $discount }}
+                            </div>
+                        @endif
+
+                        @if ( $product['badges'][0]['value'] === 'Sostenibilità')
+                            <div class="sostenibile absolute">
+                                Sostenibile
+                            </div>
+                        @endif
                     </div>
 
                     {{-- description --}}
